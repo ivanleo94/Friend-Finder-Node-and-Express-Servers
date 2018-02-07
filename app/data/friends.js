0e2c427653cccebd7f9dd641731eab6e440c34e6
ivanleo94 <31164505+ivanleo94@users.noreply.github.com>
2018-02-06 17:39:35 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+var friends = [{
+    "name" : "Zob",
+    "photo" : "http://d236bkdxj385sg.cloudfront.net/wp-content/uploads/2012/02/brian-white.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Danielle",
+    "photo" : "http://benhairstyle.com/wp-content/uploads/2014/10/beautiful_hairstyles_for_young_women.jpg",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Charu",
+    "photo" : "https://s-media-cache-ak0.pinimg.com/736x/09/0f/e7/090fe7f53967e0a0da51f12fa147e01d.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+},
+
+module.exports = friends;
